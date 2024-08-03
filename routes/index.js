4d5477e81bba7277d889859dd1e734f301cd0de9
Jake <86923926+Jake2580@users.noreply.github.com>
2024-08-04 00:33:57 +0900

    Changed the environment variable name for the Naver redirect URL.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let router = require('express').Router();
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-const redirectURI = process.env.REDIRECTURI;
+const redirectURI = process.env.REDIRECT_URI;
 const state = 'RANDOM_STATE'; // CSRF 공격 방지를 위한 상태 토큰
 
 const api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirectURI}&state=${state}`;

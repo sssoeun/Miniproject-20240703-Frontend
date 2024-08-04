e2bb6e9e7a0678fb21dd9cb256931bb695e8e324
Jake <86923926+Jake2580@users.noreply.github.com>
2024-08-04 23:42:11 +0900

    The current session is stored in MemoryStore, but in practice, it is applied to Redis rather than MemoryStore.

--- utils/session.js
+++ utils/session.js
@@ -1,8 +1,14 @@
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redis = require('redis');
+const redisClient = redis.createClient();
+
 const sessionConfig = session({
+    store: new RedisStore({ client: redisClient }),
     secret: 'qlalfdldi123698745',
     resave: false,
     saveUninitialized: false,
+    cookie: { secure: true } // HTTPS를 사용하는 경우 true로 설정
 });
 
 module.exports = { sessionConfig };

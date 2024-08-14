5908dac8710c996b6f255e724385c7b14bf4fe7e
Jake <86923926+Jake2580@users.noreply.github.com>
2024-08-14 17:18:42 +0900

    Add retry logic for Redis connection.

--- utils/session.js
+++ utils/session.js
@@ -12,9 +12,30 @@ let redisClient = createClient({
     }
 });
 
-redisClient.connect().catch(err => {
-    console.error('Could not connect to Redis', err);
-});
+let attemptCount = 0;
+let redis_connected = false; // 연결 상태를 추적하는 변수
+console.log('Connecting to Redis...');
+
+const connectWithRetry = () => {
+    redisClient.connect().then(() => {
+        console.log('Connected to Redis.');
+        redis_connected = true; // 연결 성공 시 true로 설정
+        attemptCount = 0; // 시도 횟수 초기화
+    }).catch(err => {
+        attemptCount++; // 시도 횟수 증가
+        if (attemptCount % 5 === 0) {
+            console.error('Could not connect to Redis after', attemptCount, 'attempts:', err);
+        }
+        redis_connected = false; // 연결 실패 시 false로 설정
+    }).finally(() => {
+        if (!redis_connected) {
+            redisClient.disconnect();
+            setTimeout(connectWithRetry, 5000); // 5초 후에 다시 시도
+        }
+    });
+};
+
+connectWithRetry();
 
 const sessionConfig = session({
     store: new RedisStore({ client: redisClient }),
